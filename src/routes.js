88b6f7522e2e7911aeaed36cf2e71ecdef6bec2a
Luan de Almeida <luanbianchine1@gmail.com>
2021-12-24 17:36:17 -0300

    organizando pastas

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
-import Home from './pages/principal/home';
+import Home from './principal/home';
 import About from './pages/about/about';
 import Header from './header/header';
 import Certificate from './pages/certificate/certificate';
