67a31643b9d9c98564a7db0ca8c9ca69ed34323a
Luan de Almeida <luanbianchine1@gmail.com>
2021-12-25 18:27:22 -0300

    melhorando o header e o principal

--- src/header/header.js
+++ src/header/header.js
@@ -1,23 +1,26 @@
 import React from "react";
-import { Head, Ul, Li} from './styleheader';
+import { Head, Ul, Li, Text} from './styleheader';
 import { Link } from "react-router-dom";
+import logo from '../img/principal/luan.png';
 
 function Header(){
     return(
         <Head>
-           <Link to="/"> Aqui é o header </Link>
             <Ul>
+                <Link to="/"> 
+                    <img src={logo} alt="logo"/>
+                </Link>
                 <Li>                   
-                    <Link to="/about"> Sobre mim </Link>                    
+                    <Link to="/about"> <Text> Sobre mim </Text> </Link>                    
                 </Li>
                 <Li>                 
-                   <Link to="/contact"> Contatos </Link>         
+                   <Link to="/contact"> <Text> Contatos </Text> </Link>         
                 </Li>
                 <Li>                
-                    <Link to="/hability"> Habilidades </Link>   
+                    <Link to="/hability"> <Text> Habilidades </Text> </Link>   
                 </Li>
                 <Li> 
-                    <Link to="/certificate"> Certificados </Link>  
+                    <Link to="/certificate"> <Text> Certificados </Text> </Link>  
                 </Li>
             </Ul>
         </Head>
--- src/header/styleheader.js
+++ src/header/styleheader.js
@@ -1,16 +1,30 @@
 import styled from "styled-components";
 
 export const Head = styled.div`
-  
+  background-color: #1882c5;
+  width:99.9%;
+  margin-top: -1pc;
+  margin-bottom: 10pc;
 `;
 
 export const Ul = styled.ul`
-    padding-left: 0px;
-    text-align: center;
+  padding-left: 0px;
+  text-align: init;
+  padding-top: 0.2pc;
 `;
 
 export const Li = styled.li`
-    display: inline-block;
-    padding: 10px 20px;
+  display: inline-block;
+  padding: 10px 20px;
+  position: relative;
+  top: -1.5pc;
 `;
 
+export const Text = styled.a`
+  color: white;
+  font-size: 20px;
+  :hover{
+    color: #F0502A;
+    opacity: 80%;
+  }
+`;
