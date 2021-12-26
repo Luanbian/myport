51d61d0e6bc3bc82ff360a71919a53c1a58f284e
Luan de Almeida <luanbianchine1@gmail.com>
2021-12-25 21:46:55 -0300

    adicionando uma foto de perfil

--- src/principal/home.js
+++ src/principal/home.js
@@ -1,11 +1,14 @@
 import React from "react";
-//import {Container} from './homestyled';
-import capa from "../img/principal/capa.png"
+import capa from "../img/principal/capa.png";
+import perfil from "../img/principal/ftperfil.png";
 import Projects from "../projects";
+import { Container } from "./homestyled";
 
 function Home(){
     return(
         <>
+            <Container/>
+            <img src={perfil} alt="ft-de-perfil" style={{position: 'absolute', left: '45pc', top: '1pc'}}/>
             <img src={capa} alt="capa-do-projeto" style={{width: '100%'}}/>
             <Projects/>
         </>
--- src/principal/homestyled.js
+++ src/principal/homestyled.js
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 
-export const Container = styled.img`
-    left: 3px;
+export const Container = styled.div`
+    width: 99.9%;
+    height:160px;
+    background-color: #1882c5;
+    margin-top: -10pc;
 `;
