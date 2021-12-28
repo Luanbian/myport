e025b809f82e3b36dc23305955e069183c1a0417
Luan de Almeida <luanbianchine1@gmail.com>
2021-12-27 21:02:15 -0300

    começando a estilizar o email

--- src/pages/contact/email.js
+++ src/pages/contact/email.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-//token: 0df9efde-d3a4-4ac9-aaf6-b1b6e6a2bf1b 
-
 function Email(){
     return(
         <>
             <form action="https://api.staticforms.xyz/submit" method="post">
                 <input type="hidden" name="accessKey" value="0df9efde-d3a4-4ac9-aaf6-b1b6e6a2bf1b"/>
-                <input type="text" name="name" />
+                <input type="text" name="name" placeholder='Seu nome por favor'/>
                 <input type="text" name="subject" />
                 <textarea name="message"></textarea>
                 <input type="submit" value="Submit" />
