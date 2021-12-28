30fff2cf0185f28b9358f98efd07d6675780fa12
Luan de Almeida <luanbianchine1@gmail.com>
2021-12-28 15:40:50 -0300

    estilizado minimamente a aba error e funcionando

--- src/pages/error/erro.js
+++ src/pages/error/erro.js
@@ -1,10 +1,20 @@
 import React from "react";
-
+import Imgerror from '../../styles/img/error/error.png';
+import {Imgfundo, Title, Content, Saida, Clique, Card} from './stylederro';
+import { Link } from "react-router-dom";
 function Erro(){
     return(
-        <h1>
-            Você saiu da casinha
-        </h1>
+        <>
+            <Imgfundo src={Imgerror} alt="img-undraw-error"/>
+            <Card>
+                <Title>vish...</Title>
+                <Content>Parece que você explorou bem longe</Content>
+                <Saida>Sem problemas, você pode voltar a explorar o site sem medo</Saida>
+                <Link to="/">
+                    <Clique>Clique aqui para voltar a pagina principal</Clique>
+                </Link>
+            </Card>
+        </>
     )
 }
 
---
+++ src/pages/error/stylederro.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const Imgfundo = styled.img`
+    margin-left: 22%;
+`;
+
+export const Title = styled.h1`
+
+`;
+
+export const Content = styled.h2`
+
+`;
+
+export const Saida = styled.h3`
+
+`;
+
+export const Clique = styled.a`
+
+`;
+
+export const Card = styled.div`
+    border: solid 5px red;
+    width: 700px;
+    height: 500px;
+    margin-left: 50px;
+    margin-top:-40pc;
+`;
