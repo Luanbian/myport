1c68216c935164555351c47ab7422c5444b68944
Luan de Almeida <luanbianchine1@gmail.com>
2022-01-16 18:01:53 -0300

    esqueleto de habilidades pronto

--- src/pages/habilits/hability.js
+++ src/pages/habilits/hability.js
@@ -1,26 +1,44 @@
 import React, { Suspense } from "react";
-import {Wait, CardBackend, CardFrontend, CardTotal} from './styledhabilits';
+import {Wait, CardBackend, CardFrontend, CardTotal, Title, Description, Card, Intro, HardSkill, SoftSkills, CardSoft, CardMobile} from './styledhabilits';
 import { Canvas } from '@react-three/fiber';
 import Earth from "../../components/sphere/sphere";
-
 
 function Hability(){
     return(
         <>
-            <Wait>
-                <Canvas>
-                    <Suspense fallback={null}>
-                        <Earth />
-                    </Suspense>
-                </Canvas>
-            </Wait>
+            <Card>
+                <Wait>
+                    <Canvas>
+                        <Suspense fallback={null}>
+                            <Earth /> 
+                        </Suspense>
+                    </Canvas>
+                </Wait>
+                <Intro>
+                    <Title>
+                        Habilidades
+                    </Title>
+                    <Description>
+                        Lorem lorem 
+                    </Description>
+                </Intro>
+            </Card>
+
             <CardTotal>
+                <HardSkill>Hard Skills</HardSkill>
                 <CardFrontend>
 
                 </CardFrontend>
                 <CardBackend>
 
                 </CardBackend>
+                <CardMobile>
+
+                </CardMobile>
+                <SoftSkills>Soft Skills</SoftSkills>
+                <CardSoft>
+
+                </CardSoft>
             </CardTotal>
         </>
     )
--- src/pages/habilits/styledhabilits.js
+++ src/pages/habilits/styledhabilits.js
@@ -19,3 +19,42 @@ export const CardFrontend = styled.div`
 export const CardTotal = styled.div` 
 
 `;
+
+export const Title = styled.h1` 
+   font-size: 60px;
+   margin-top: -50px;
+   margin-left: -90px;
+`;
+
+export const Description = styled.p` 
+   font-size: 30px;
+`;
+
+export const Card = styled.div` 
+   display: flex;
+   flex-direction: row;
+`;
+
+export const Intro = styled.div` 
+   flex-direction: column;
+`;
+
+export const HardSkill = styled.h1` 
+   font-size: 50px;
+   display: flex;
+   justify-content: center;
+`;
+
+export const SoftSkills = styled.h1` 
+   font-size: 50px;
+   display: flex;
+   justify-content: center;
+`;
+
+export const CardSoft = styled.div` 
+
+`;
+
+export const CardMobile = styled.div` 
+
+`;
